41fdaa3585e2dd653e1424cde5fd677ebc72b9b2
firekind <shyamant.achar@gmail.com>
2020-06-07 18:52:59 +0530

    added login tokens, update /upload route, added /patient/add route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,33 @@
 const multer = require('multer');
 const config = require('../config.json');
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
+const path = require('path');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
-// defining the names of the files and the number
-// of the files that can be uploaded
-const upload = multer();
+// getting secret that is used to create auth tokens
+const secret = process.env.SECRET || config.default_secret
+
+// defining the properties (such as storage location, name and number
+// of files that can be uploaded etc.) of the files that can be uploaded.
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb) {
+            // constructing path to folder
+            const folder = path.join(config.storage, req.body["patient_id"]);
+
+            // creating folder
+            fs.mkdirSync(folder, { recursive: true });
+
+            // calling callback
+            cb(null, folder);
+        },
+        filename: function(req, file, cb) {
+            cb(null, file.originalname);
+        }
+    })
+});
 const uploadType = upload.fields([
     {name: 't1', maxCount: 1},
     {name: 't1ce', maxCount: 1},
@@ -13,25 +36,15 @@ const uploadType = upload.fields([
     {name: 'seg', maxCount: 1},
 ]);
 
-function generateID() {
-    const max = 1000;
-    const min = 0;
-    return Math.floor (
-        Math.random() * (max - min) + min
-    );
-}
+const generateID = () => crypto.randomBytes(3).toString("hex");
+const createToken = ({email, id}) => jwt.sign({
+     email: email,
+    _id: id
+}, secret, {
+    expiresIn: "24h"
+})
 
 module.exports = function(client, router) {
-
-    /**
-     * End point to receive MRI scans.
-     */
-    router.post('/upload', uploadType, function (req, res) {
-        // TODO: check validity of token
-
-        // redirecting request to model server
-        res.redirect(307, `${config.model_server}/api/upload`);
-    });
 
     /**
      * End point that is used to login. On a successful login, returns
@@ -59,9 +72,10 @@ module.exports = function(client, router) {
 
                     // if they match, return token
                     if (result === true) {
+                        // sending response
                         res.status(200).json({
                             success: true,
-                            token: "some-super-secret-token" // TODO: send an actual token
+                            token: createToken({email: reply.email, id: id})
                         });
                     } else {
 
@@ -106,18 +120,88 @@ module.exports = function(client, router) {
                 // else, send token.
                 res.status(200).json({
                     success: true,
-                    token: "some-super-secret-token" // TODO: send an actual token
+                    token: createToken({email: email, id: id})
                 });
             })
         });
     })
 
     /**
+     * Middleware to check validity of token
+     */
+    router.use(function(req, res, next) {
+        // getting token from the request body, parameter or header
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+        if (token) {
+            // verifying token
+            jwt.verify(token, secret, function(err, decoded) {
+                if (err) {
+                    // The token has expired or is invalid
+                    // noinspection JSUnresolvedFunction
+                    res.status(401).json({
+                        success: false,
+                        message: "Invalid token."
+                    })
+                }
+
+                // adding the token contents in the req object
+                req.token_contents = decoded;
+
+                // calling the next route
+                next();
+            })
+        }
+        else {
+            // no token provided
+            // noinspection JSUnresolvedFunction
+            res.status(401).json({
+                success: false,
+                message: "No token provided"
+            })
+        }
+    })
+
+    /**
+     * End point to receive MRI scans.
+     */
+    router.post('/upload', uploadType, function (req, res) {
+
+        // generating id for the medical record
+        const id = generateID();
+
+        // adding path to patient key in db
+        client.hmset(`${config.record_prefix}:${id}`, [
+            'mri_scans', path.join(config.storage, req.body['patient_id']).toString()
+        ], function(err) {
+            // if error occurred while adding, send error message with
+            // http status code 500 (Internal server error)
+            if (err)
+                res.status(500).json({
+                    success: false,
+                    message: err.toString()
+                });
+
+            // else, send record id.
+            res.status(200).json({
+                success: true,
+                record_id: id
+            });
+        });
+
+        // redirecting request to model server
+        res.redirect(307, `${config.model_server}/api/upload`);
+    });
+
+    /**
      * End point that is used to delete a user.
      */
     router.delete('/user/delete', function(req, res) {
+        // getting the token contents from request
+        const { token_contents } = req;
+
         // creating the key that is used to reference the user in the database
-        const key = `${config.user_prefix}:${req.query.id}`
+        const key = `${config.user_prefix}:${token_contents["_id"]}`
 
         // deleting user
         client.del(key, function(err) {
@@ -140,16 +224,47 @@ module.exports = function(client, router) {
      * End point to add a patient
      */
     router.post('/patient/add', function(req, res) {
+        const { name, gender, dob, contact } = req.body;
+        const id = generateID();
+        const key = `patients:${id}`;
 
+        client.hmset(key, [
+            'name', name,
+            'gender', gender,
+            'dob', dob,
+            'contact', contact
+        ], function(err) {
+            // if error occurred while adding, send error message with
+            // http status code 500 (Internal server error)
+            if (err)
+                res.status(500).json({
+                    success: false,
+                    message: err.toString()
+                });
+
+            // else, send patient id.
+            res.status(200).json({
+                success: true,
+                patient_id: id
+            });
+        });
     });
 
     /**
-     * End point to add a medical record
+     * End point to update a medical record
      */
-    router.post("/medical-record/add", function(req, res) {
-        const { id, doctor_id, patient_id, diagnosis, symptoms, treatment} = req.body;
+    router.post("/medical-record/update", function(req, res) {
+        // getting info from body
+        const { record_id, patient_id, diagnosis, symptoms, treatment } = req.body;
+
+        // getting contents of token from request
+        const { token_contents } = req;
+
+        // getting doctor id (user id) from token
+        const doctor_id = token_contents["_id"]
+
         // adding record entry to database
-        client.hmset(`${config.record_prefix}:${id}`, [
+        client.hmset(`${config.record_prefix}:${record_id}`, [
             'doctor_id', doctor_id,
             'patient_id', patient_id,
             'diagnosis', diagnosis,
